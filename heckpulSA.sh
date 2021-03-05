cc25c7546f3c9c801f26e3e6c3c59b787559d2e3
MR M3R4Z <77550679+Ardi12345X@users.noreply.github.com>
2021-03-06 01:53:04 +0700

    Add files via upload

---
+++ heckpulSA.sh
@@ -0,0 +1,76 @@
+#jangan ganti author nya"
+#yang ganti author nya semoga besoknya bisa buat script sendiri"
+#subscribe chennel MR M3R4Z"
+#script ini tidak work :("
+sleep 3
+echo -e "\e[93mhalo\e[0m";
+sleep 3
+echo -e "\e[32mheck pulsa\e[0m";
+sleep 2
+echo -e "\e[95mhecked by M3R4Z\e[0m";
+sleep 4
+echo -e "\e[33msubscribe chennel MR M3R4Z\e[0m";
+sleep 3
+echo -e "\e[92m<<<<<<<<<<<heck pulsa>>>>>>>>>>>\e[0m";
+sleep 3
+echo -e "\e[35mAuthor.=MR M3R4Z\e[0m";
+sleep 2
+echo -e "\e[95mno WA. =+6283192644730\e[0m";
+sleep 3
+echo -e "\e[90mgrup.  =TCT=Termux•Cyber•Tim\e[0m";
+sleep 6
+echo -e "\e[92mheck pulsa\e[0m";
+sleep 3
+echo -e "\e[93mmasukan nomor\e[0m";
+sleep 2
+read -p '$>' userinput1;
+sleep 3
+echo -e "\e[33mmasukan jumlah pulsa\e[0m";
+read -p '$>' userinput1;
+#
+sleep 5
+echo -e "\e[93mtunggu sebentar\e[0m";
+sleep 9
+echo -e "\e[36mloading\e[0m";
+sleep 6
+echo -e "\e[32mloading\e[0m";
+sleep 4
+echo -e "\e[92mloading\e[0m";
+sleep 9
+echo -e "\e[91mloading\e[0m";
+sleep 5
+echo -e "\e[93mmasukan kembali nomor \e[0m";
+sleep 2
+read -p '$>' userinput2;
+#
+sleep 11
+echo -e "\e[34mheck pulsa..........\e[0m";
+sleep 5
+echo -e "\e[92mheck dimulai\e[0m";
+sleep 5
+echo -e "\e[96mketik y untuk lanjut\e[0m";
+read -p '$>' userinput3;
+#
+sleep 5
+echo -e "\e[33mloading10%\e[0m";
+sleep 3
+echo -e "\e[94mloading20%\e[0m";
+sleep 9
+echo -e "\e[91mloading40%\e[0m";
+sleep 6
+echo -e "\e[90mloading60%\e[0m";
+sleep 3
+echo -e "\e[92mloading80%\e[0m";
+sleep 12
+echo -e "\e[31mloading100%\e[0m";
+sleep 5
+echo -e "\e[32mheck pulsa berhasil\e[0m";
+sleep 3
+echo -e "\e[90mtunggu 10 menit \e[0m";
+sleep 5
+echo -e "\e[35mketik exit untuk keluar\e[0m";
+read -p '$>' userinput4;
+#
+sleep 6
+echo -e "\e[91msubscribe chennel MR M3R4Z\e[0m";
+sleep 5
